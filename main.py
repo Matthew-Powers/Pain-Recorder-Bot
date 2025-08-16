6e80a177432d7b5f12c5514769cc6acd25813038
Matthew-Powers <mpowers16@ewu.edu>
2025-08-16 11:52:29 -0700

    Made a simple discord bot that takes a single command and asks the user to rate their pain levels from 0 to 10, and then reminds said user every 3 hours to do so a total of 5 times. Every 6 hours, it also reminds the user to take another pain pill

---
+++ main.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+import datetime
+import asyncio
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+waitTime = datetime.timedelta(hours=3).total_seconds()
+
+@bot.event
+async def on_ready():
+    print('Bot online')
+
+@bot.command()
+async def pain(ctx):
+    for counter in range(0, 5):
+        embed = discord.Embed(title='Pain Level', description='What is your pain level?')
+        poll_message = await ctx.send(embed=embed)
+        await poll_message.add_reaction("0️⃣")
+        await poll_message.add_reaction("1️⃣")
+        await poll_message.add_reaction("2️⃣")
+        await poll_message.add_reaction("3️⃣")
+        await poll_message.add_reaction("4️⃣")
+        await poll_message.add_reaction("5️⃣")
+        await poll_message.add_reaction("6️⃣")
+        await poll_message.add_reaction("7️⃣")
+        await poll_message.add_reaction("8️⃣")
+        await poll_message.add_reaction("9️⃣")
+        await poll_message.add_reaction("🔟")
+        if counter % 2 == 0 and counter != 0:
+            await ctx.reply("Record your current pain level and take your next pill")
+        else:
+            await ctx.reply("Record your current pain level")
+        await asyncio.sleep(waitTime)
+
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
